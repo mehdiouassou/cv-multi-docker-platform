8a07d9b09c9b00c74437d246c0ef82508d8c067a
mehdiouassou <ouassoumehdidev@gmail.com>
2026-02-21 00:36:00 +0100

    chore: pulizia repository e configurazione pacchetto Python

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="symphony-cv-platform",
+    version="1.0.0",
+    author="Studente",
+    description="Piattaforma Multi-Docker per Algoritmi di Computer Vision",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi>=0.100.0",
+        "uvicorn>=0.20.0",
+        "docker>=6.1.0",
+        "pydantic>=2.0.0",
+        "PyYAML>=6.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
